3ab49d0f898904f73d2481c1f72a7955610e8960
neil araujo <neilrocks999@gmail.com>
2020-04-02 18:36:46 +0530

    Added activity tracker

---
+++ app/src/main/java/com/example/android/covidhack/ProbabilityActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.covidhack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProbabilityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_probability);
+    }
+}
